8a0597ba77f37b1445e0a973fc74b4dce30b2925
Mayara Araujo <63872581+mayaramaaraujo@users.noreply.github.com>
2022-04-18 17:23:31 -0300

    Add files via upload

--- 0x00/personagem/Personagem.java
+++ 0x00/personagem/Personagem.java
@@ -59,7 +59,7 @@ public abstract class Personagem {
                 ", vigor = " + vigor +
                 ", resistencia = " + resistencia +
                 ", destreza = " + destreza +
-                ", dano ataque=" + String.format(Locale.FRANCE,"%,.2f", getDanoAtaque()) +
+                ", dano ataque = " + String.format(Locale.FRANCE,"%,.2f", getDanoAtaque()) +
                 " }";
     }
 
