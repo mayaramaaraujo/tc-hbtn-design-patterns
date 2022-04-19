e2e8ec6323f1eba1f1f02fe0a621beed5138429f
Mayara Araujo <63872581+mayaramaaraujo@users.noreply.github.com>
2022-04-19 08:05:36 -0300

    Add files via upload

---
+++ 0x01/salario/Program.java
@@ -0,0 +1,17 @@
+import java.util.Locale;
+
+public class Program {
+    public static void main(String[] args) {
+        SalarioService salarioService1 = new SalarioService();
+        SalarioService salarioService2 = new SalarioService();
+
+        double valorSalario1 = salarioService1.calcular(1200, 150, 3000, 10);
+        double valorSalario2 = salarioService2.calcular(2500, 350, 10000, 15);
+
+        System.out.printf(Locale.FRANCE, "Salario 1: %.2f\n", valorSalario1);
+        System.out.printf(Locale.FRANCE,"UUID 1: %s\n\n", salarioService1.getUuid());
+
+        System.out.printf(Locale.FRANCE,"Salario 2: %.2f\n", valorSalario2);
+        System.out.printf(Locale.FRANCE,"UUID 2: %s\n", salarioService2.getUuid());
+    }
+}
