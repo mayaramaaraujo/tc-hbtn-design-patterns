1336b5de97240d6702cc3cf6653cef8ae8926f3b
Mayara Araujo <63872581+mayaramaaraujo@users.noreply.github.com>
2022-04-26 11:35:26 -0300

    Add files via upload

--- 0x02/impressao/ImpressaoFactory.java
+++ 0x02/impressao/ImpressaoFactory.java
@@ -1,6 +1,6 @@
 public class ImpressaoFactory {
 
-    static Impressao createImpressao(TamanhoImpressao tamanhoImpressao, int paginasTotais, int paginasColoridas, boolean ehFrenteVerso) {
+    public static Impressao createImpressao(TamanhoImpressao tamanhoImpressao, int paginasTotais, int paginasColoridas, boolean ehFrenteVerso) {
         return new Impressao(tamanhoImpressao,paginasTotais,paginasColoridas,ehFrenteVerso);
     }
 }
