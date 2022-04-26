f119cadaa6af796001e22d9e923aaac33dc042a5
Mayara Araujo <63872581+mayaramaaraujo@users.noreply.github.com>
2022-04-26 10:54:30 -0300

    Add files via upload

--- 0x02/lanchonete/Pedido.java
+++ 0x02/lanchonete/Pedido.java
@@ -25,14 +25,18 @@ public class Pedido {
 
     @Override
     public String toString() {
+        String foraDaCaixa = "\tFora da Caixa:\n";
 
-        return "\tFora da Caixa:\n" +
-                itensForaCaixa.toString().replace("[","")
-                        .replace("]", "")
-                        .replace(",", "\n") + "\n" +
-                "\tDentro da Caixa:\n" +
-                itensDentroCaixa.toString().replace("[","")
-                        .replace("]", "")
-                        .replace(",", "\n") + "\n";
+        for (ItemPedido item : itensForaCaixa){
+            foraDaCaixa += String.format("\t\t- %s %s\n", item.getTipo(), item.getNome());
+        }
+
+        String dentroDaCaixa = "\tDentro da Caixa:\n";
+
+        for (ItemPedido item : itensDentroCaixa){
+            dentroDaCaixa += String.format("\t\t- %s %s\n", item.getTipo(), item.getNome());
+        }
+
+        return foraDaCaixa + dentroDaCaixa;
     }
 }
