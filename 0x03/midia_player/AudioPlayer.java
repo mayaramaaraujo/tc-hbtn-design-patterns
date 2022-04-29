c6e579509fc514b6f193d8a8b5098b928b44b458
Mayara Araujo <63872581+mayaramaaraujo@users.noreply.github.com>
2022-04-29 14:48:13 -0300

    Add files via upload

--- 0x03/midia_player/AudioPlayer.java
+++ 0x03/midia_player/AudioPlayer.java
@@ -3,7 +3,7 @@ public class AudioPlayer implements MediaPlayer {
     @Override
     public void reproduzir(TipoMedia tipoMedia,String nomeDoArquivo) {
         if(tipoMedia == TipoMedia.MP3) {
-            System.out.println("Reproduzindo MP3: " + nomeDoArquivo);
+            System.out.printf("Reproduzindo MP3: " + nomeDoArquivo);
         } else {
             MediaAdapter mediaAdapter = new MediaAdapter();
             mediaAdapter.reproduzir(tipoMedia, nomeDoArquivo);
